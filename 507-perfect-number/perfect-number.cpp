677dc37533a9fa911371f9eea7510e41a42e8593
RishavKumar91 <rishavyadav15@zohomail.in>
2026-01-16 00:44:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.93%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -4,14 +4,10 @@ public:
         if (num == 1) return false;
 
         int sum = 1;
-        for (int i = 2; i * 1LL * i <= num; i++) {
-            if (num % i == 0) {
-                sum += i;
-                if (i != num / i)
-                    sum += num / i;
-            }
+        for (int i = 2; i<=sqrt(num); i++) {
+            if (num % i == 0)  sum += i;
+            if (num%i==0 && i != num / i)  sum += num / i;
         }
-
         return sum == num;
     }
 };
