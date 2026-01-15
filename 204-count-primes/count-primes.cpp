d238381a3d864900553450753bdf19583c8a677d
RishavKumar91 <rishavyadav15@zohomail.in>
2026-01-16 02:04:54 +0530

    Time: 139 ms (91.98%) | Memory: 132.6 MB (14.63%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -4,7 +4,7 @@ public:
         if(n<2) return 0;
         vector<int> v(n,0);int count=0;
         v[0]=v[1]=1;
-        for(int i=2;i*i<=n;i++){
+        for(int i=2;i*i<n;i++){
             if(v[i]==0){
                 for(int j = i+i;j<n;j+=i)
                 v[j] = 1;
