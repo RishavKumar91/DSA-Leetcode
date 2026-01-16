aecae06c7958bef56c53f3917988ae883e14dcc0
RishavKumar91 <rishavyadav15@zohomail.in>
2026-01-17 03:51:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (44.00%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int addDigits(int num) {
-        int sum = num;
-        while(sum >9){
-            num =sum;
-            sum =0;
+        while(num >9){
+            int sum =0;
             while(num!=0){
                 int r = num%    10;
                 sum = sum+r;
                 num/=10;
             }
+        num=sum;
         }
-        return sum;
+        return num;
     }
 };
