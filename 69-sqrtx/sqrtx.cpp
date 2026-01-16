08fff5e74e0668de0792b00db619ec6dc9325efe
RishavKumar91 <rishavyadav15@zohomail.in>
2026-01-17 04:32:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (84.40%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int y = sqrt(x);
-        return y;
-        
-        return 0;
+        // int y = sqrt(x);
+        // return y;
+        int f =1,l=x;
+        while(f<=l){
+            long long m=f+(l-f)/2;
+            if(m*m==x) return m;
+            if(m*m<x)  f=m+1;
+            else l = m-1;
+        }
+        return l;
     }
 };
