343cbf4df53321b1fb9bcdb0aa2ba0972071f4c5
RishavKumar91 <rishavyadav15@zohomail.in>
2026-01-17 16:17:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (13.76%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double myPow(double x, long long n) {
+        long long N = n;
+        if(x==1.0) return 1.0;
+        
+        if(N==0) return 1.0;
+        if(N<0) return myPow(1/x,-N);
+        double h= myPow(x,N/2);
+        if(N%2==0) return h*h;
+        else       return h*h * x;
+    }
+};
