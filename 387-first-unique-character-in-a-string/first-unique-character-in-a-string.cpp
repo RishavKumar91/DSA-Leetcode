849eec0a1a497af4d356db025f257343b820630a
RishavKumar91 <rishavyadav15@zohomail.in>
2026-01-17 12:43:39 +0530

    Time: 1 ms (84.86%) | Memory: 14.6 MB (71.80%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> v(26,0);
+        for(int i =0;i<s.size();i++){
+            v[s[i]-97]++;
+        }
+        for(int i =0;i<s.size();i++)
+        if(v[s[i]-97]==1) return i;
+        return -1;
+
+    }
+};
