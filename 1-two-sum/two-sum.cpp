8dd5a1acf20b85fe5fa534615733388e11e4fe4f
RishavKumar91 <rishavyadav15@zohomail.in>
2026-01-21 20:24:23 +0530

    Time: 11 ms (39.07%) | Memory: 16.6 MB (5.81%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        priority_queue<int> maxHeap;
-        vector<int> v; int n = nums.size();
-        priority_queue<int, vector<int>, greater<int>> minHeap;
+        // priority_queue<int> maxHeap;
+        // vector<int> v; int n = nums.size();
+        // priority_queue<int, vector<int>, greater<int>> minHeap;
+        // for(int i =0;i<n;i++){
+        //     maxHeap.push(nums[i]);
+        //     minHeap.push(nums[i]);
+        // }
+        // while(minHeap.empty() ==0 || maxHeap.empty()==0){
+        //     int a = 0;
+        //     if(minHeap.size()) a+=minHeap.top();
+        //     if(maxHeap.size()) a+=maxHeap.top();
+        //     if(a==target) {v.push_back(minHeap.top()) ; v.push_back(maxHeap.top());break;}
+        //     else if(a>target) maxHeap.pop();
+        //     else minHeap.pop();
+        // }
+        // vector<int> ans;
+        // for(int i =0;i<n;i++){
+        //     if(nums[i]==v[0]||nums[i]==v[1]) ans.push_back(i) ; 
+        // }
+        int n =nums.size();
+        vector<int> v;
+        unordered_map<int,int> mp;
+        for(int i =0;i<n;i++)
+        mp[nums[i]] =i;
         for(int i =0;i<n;i++){
-            maxHeap.push(nums[i]);
-            minHeap.push(nums[i]);
+            int a  = target-nums[i];
+            if(a==nums[i] && i!=mp[nums[i]]) {v.push_back(i); v.push_back(mp[nums[i]]);break;}
+            mp.erase(nums[i]);
+            if(mp.find(a) != mp.end()){ v.push_back(i) ; v.push_back(mp[a]); break;}
+            mp[nums[i]]=i;
         }
-        while(minHeap.empty() ==0 || maxHeap.empty()==0){
-            int a = 0;
-            if(minHeap.size()) a+=minHeap.top();
-            if(maxHeap.size()) a+=maxHeap.top();
-            if(a==target) {v.push_back(minHeap.top()) ; v.push_back(maxHeap.top());break;}
-            else if(a>target) maxHeap.pop();
-            else minHeap.pop();
-        }
-        vector<int> ans;
-        for(int i =0;i<n;i++){
-            if(nums[i]==v[0]||nums[i]==v[1]) ans.push_back(i) ; 
-        }
-        return ans;
+        return v;
     }
 };
