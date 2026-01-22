e36f9a3aeee268201acdfc5cad1d71eec9b5610e
RishavKumar91 <rishavyadav15@zohomail.in>
2026-01-22 17:33:51 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (10.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t) return 1;
-        else return 0;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // if(s==t) return 1;
+        // else return 0;
+
+        int n = s.size();
+        if(n!=t.size()) return 0;
+        unordered_map<char ,int> mp;
+        for(int i =0;i<n;i++){
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+        for(int i =0;i<n;i++)
+        if(mp[s[i]] !=0) return 0;
+    return 1;
     }
 };
