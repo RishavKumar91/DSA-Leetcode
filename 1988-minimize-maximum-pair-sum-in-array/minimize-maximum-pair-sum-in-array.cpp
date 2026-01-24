f003142869685c9e1848637bcc3e7572a24e48db
RishavKumar91 <rishavyadav15@zohomail.in>
2026-01-24 18:03:16 +0530

    Time: 293 ms (5.47%) | Memory: 121.7 MB (5.00%) - LeetSync

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     int minPairSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n = nums.size(),maxx = 0;
+        // sort(nums.begin(),nums.end());
+        int n = nums.size();//maxx = 0;
+        // for(int i =0;i<n/2;i++){
+        //     maxx = max(maxx,nums[i] + nums[n-i-1]);
+        // }
+        // return maxx;
+        priority_queue<int> maxH;
+        priority_queue<int,vector<int>,greater<int>> minH;
+        for(int i = 0;i<n;i++){
+            maxH.push(nums[i]);
+            minH.push(nums[i]);
+        }
+        int maxx = 0;
         for(int i =0;i<n/2;i++){
-            maxx = max(maxx,nums[i] + nums[n-i-1]);
+            maxx = max(maxx,maxH.top()+minH.top());
+            minH.pop() ; maxH.pop();
         }
         return maxx;
     }
