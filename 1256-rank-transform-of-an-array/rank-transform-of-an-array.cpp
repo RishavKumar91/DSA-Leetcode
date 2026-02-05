ca03699fea90797a3c31a58c49bfccfcfad6b06f
RishavKumar91 <rishavyadav15@zohomail.in>
2026-02-05 13:56:00 +0530

    Time: 39 ms (52.73%) | Memory: 43.9 MB (61.33%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -2,20 +2,34 @@ class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
         int n = arr.size();
+    //     unordered_map<int,int> mp;
+    //     priority_queue<int, vector<int>, greater<int>> pq;
+    //     for(int i =0;i<n;i++){
+    //         pq.push(arr[i]);
+    //     }
+    //     int i =1;
+    //     while(pq.size()){
+    //         if(mp.find(pq.top()) == mp.end() ) mp[pq.top()] = i++;
+    //         pq.pop();
+    //     }
+    //     vector<int> ans(n,-1);
+    //     for(int i =0;i<n;i++){
+    //         ans[i] = mp[arr[i]];
+    //     }
+    // return ans;
+
+        
         unordered_map<int,int> mp;
-        priority_queue<int, vector<int>, greater<int>> pq;
+        vector<int> dummy = arr;
+        int k =1;
+        sort(dummy.begin(),dummy.end());
         for(int i =0;i<n;i++){
-            pq.push(arr[i]);
-        }
-        int i =1;
-        while(pq.size()){
-            if(mp.find(pq.top()) == mp.end() ) mp[pq.top()] = i++;
-            pq.pop();
+            if(mp.find(dummy[i])==mp.end()) mp[dummy[i]] =k++;
         }
         vector<int> ans(n,-1);
         for(int i =0;i<n;i++){
             ans[i] = mp[arr[i]];
         }
-    return ans;
+        return ans;
     }
 };
