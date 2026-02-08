e1ac1ee6238739eedb82f61b4dfd1f454d84e578
RishavKumar91 <rishavyadav15@zohomail.in>
2026-02-08 23:18:39 +0530

    Time: 3 ms (16.59%) | Memory: 8.4 MB (35.36%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> s;
+        while(n!=1){
+            if(s.count(n)) return 0;
+            s.insert(n);
+            int sum = 0;
+            while(n!=0){
+                int r = n%10;
+                n/=10;
+                sum+=(r*r);
+            }
+            n = sum;
+        }
+        return 1;
+    }
+};
