730d27973b45825a6f8ea9c9e9645190cf044b46
RishavKumar91 <rishavyadav15@zohomail.in>
2026-02-11 01:05:16 +0530

    Time: 0 ms (100.00%) | Memory: 32.2 MB (51.47%) - LeetSync

--- 4214-count-dominant-indices/count-dominant-indices.cpp
+++ 4214-count-dominant-indices/count-dominant-indices.cpp
@@ -2,19 +2,13 @@ class Solution {
 public:
     int dominantIndices(vector<int>& nums) {
         int n = nums.size();
-
         int total = 0;
         for (int x : nums) total += x;
-
         int rightSum = total;
         int count = 0;
-
         for (int i = 0; i < n - 1; i++) {
-            rightSum -= nums[i];   // sum of elements to the right
+            rightSum -= nums[i];
             int len = n - i - 1;
-
-            // nums[i] > rightSum / len
-            // multiply both sides
             if ((long long)nums[i] * len > rightSum)
                 count++;
         }
