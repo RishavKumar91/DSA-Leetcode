3a5b6a7495616a7ac35dfd19ab7eb45d5a0818fe
RishavKumar91 <rishavyadav15@zohomail.in>
2026-02-13 01:59:53 +0530

    Time: 213 ms (40.59%) | Memory: 25.5 MB (53.32%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -4,22 +4,19 @@ bool chck(vector<int> &v){
         int freq = 0;
 
         for(int i = 0; i < 26; i++){
-            if(v[i] != 0){
-                if(freq == 0)
-                    freq = v[i];
-                else if(v[i] != freq)
-                    return false;
-            }
+            if(v[i] == 0 ) continue;
+            if(freq==0 )
+                freq = v[i];
+            if(v[i] != freq)
+                return false;
         }
-        return freq != 0;
-    }
+        return 1;
+}
     int longestBalanced(string s) {
         int maxans = 0;
         int n = s.size();
-        if(n==1) return 1;
         for(int i=0;i<n;i++){
             vector<int> v(26,0);
-            int j = i ;
             for(int j =i;j<n;j++){
                 v[s[j]-'a']++;
                 if(chck(v) == 1) maxans = max(maxans,j-i+1);
