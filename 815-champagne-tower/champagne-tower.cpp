7ef04720c48da2f8473fb5682b7587efee647995
RishavKumar91 <rishavyadav15@zohomail.in>
2026-02-14 15:26:18 +0530

    Time: 11 ms (25.42%) | Memory: 45.5 MB (22.03%) - LeetSync

---
+++ 815-champagne-tower/champagne-tower.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<double>> dp(100, vector<double>(100, 0));
+        double p = poured;
+        dp[0][0] = p;
+        for(int i =0;i<99;i++){
+            for(int j = 0;j<=i;j++){
+                if(dp[i][j]>1.0){
+                    double remhalf = (dp[i][j] - 1.0)/2;
+                    dp[i][j] = 1.0;
+                    dp[i+1][j] += remhalf;
+                    dp[i+1][j+1] +=remhalf;
+                }
+            }
+        }
+    return dp[query_row][query_glass];
+    }
+};
