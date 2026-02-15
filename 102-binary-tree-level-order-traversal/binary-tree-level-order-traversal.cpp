6f9c7decf054d778b28928c70e050ea9b7afd7ca
RishavKumar91 <rishavyadav15@zohomail.in>
2026-02-15 17:38:53 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (98.83%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -15,22 +15,15 @@ public:
         if(root==NULL) return 0;
         return 1 + max(lvl(root->right),lvl(root->left));
     }
-    void lvlbylvl(TreeNode *root,int currlvl,int maxlvl,vector<vector<int>> &ans,vector<int> &v){
+    void all(TreeNode *root,vector<vector<int>> &ans,int currlvl){
         if(root==NULL) return ;
-        if(maxlvl==currlvl) v.push_back(root->val);
-        lvlbylvl(root->left,currlvl+1,maxlvl,ans,v);
-        lvlbylvl(root->right,currlvl+1,maxlvl,ans,v);
-    }
-    void all(TreeNode * root, vector<vector<int>> &ans){
-        for(int i =1;i<=lvl(root);i++){
-            vector<int> v;
-            lvlbylvl(root,1,i,ans,v);
-            ans.push_back(v);
-        }
+        ans[currlvl].push_back(root->val);
+        all(root->left,ans,currlvl+1);
+        all(root->right,ans,currlvl+1);
     }
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-        all(root,ans);
+        vector<vector<int>> ans(lvl(root));
+        all(root,ans,0);
         return ans;
     }
 };
