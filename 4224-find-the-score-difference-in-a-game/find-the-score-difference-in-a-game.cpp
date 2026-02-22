888eef6e910d289bed45a62650aadd215090a825
RishavKumar91 <rishavyadav15@zohomail.in>
2026-02-23 01:37:51 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (5.88%) - LeetSync

--- 4224-find-the-score-difference-in-a-game/find-the-score-difference-in-a-game.cpp
+++ 4224-find-the-score-difference-in-a-game/find-the-score-difference-in-a-game.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int scoreDifference(vector<int>& nums) {
-        int n = nums.size();
-        int playr1pnt = 0;
-        int playr2pnt = 0;
-        bool activ1 = 1;
-        bool activ2 = 0;
-        for(int i =0;i<n;i++){
+        int ans = 0;
+        bool activ = 1;
+        for(int i =0;i<nums.size();i++){
             if(nums[i]%2==1 && (i+1)%6==0  ) ; //swap(activ1,activ2);
-            else if(nums[i]%2==1 || (i+1)%6==0  ) swap(activ1,activ2);
-            if(activ1==1 ) playr1pnt+=nums[i];
-            else playr2pnt+=nums[i];
+            else if(nums[i]%2==1 || (i+1)%6==0  ) activ = !activ;
+            if(activ==1 ) ans+=nums[i];
+            else ans-=nums[i];
         }
-    return playr1pnt-playr2pnt;
+    return ans;
     }
 };
