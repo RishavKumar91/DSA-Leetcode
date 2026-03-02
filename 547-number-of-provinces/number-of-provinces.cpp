5f46f6ee5c4f1b32c4abc7bbb57aabcc101802c5
RishavKumar91 <rishavyadav15@zohomail.in>
2026-03-02 16:19:52 +0530

    Time: 15 ms (6.16%) | Memory: 20.3 MB (40.30%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
-vector<int> parnt;
-vector<int> siz;
-void make(int i ){
-    parnt[i] = i; siz[i]=1;
-}
-int find(int i ){
-    if(parnt[i]==i) return i;
-    return parnt[i]= find(parnt[i]);
-}
-void Union(int u,int v){
-    u = find(u);
-    v = find(v);
-    if(u!=v){
-        if(siz[u]>=siz[v]) { parnt[v] =u ; siz[u] += siz[v];}
-        else { parnt[u] = v ; siz[v] += siz[u];}
-    }
-}
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        parnt.resize(n);
-        siz.resize(n);
-        for(int i =0;i<n;i++){
-            make(i);
-        }
-        for(int i =0;i<n;i++){
-            for(int j =0;j<n;j++){
-                if(i!=j && isConnected[i][j]==1){
-                    Union(i,j);
-                }
+    void dfs(vector<int> &visit ,vector<vector<int>>& isConnected,int start){
+        queue<int> q;
+        int n = isConnected.size() ;
+        q.push(start);
+        while(!q.empty()){
+            int now = q.front();
+            q.pop();
+            visit[now]  = 1;
+            for(int i =0;i<n;i++){
+                if(i!=now && isConnected[now][i] ==  1 && visit[i] == 0) q.push(i);
             }
         }
-        int  ans =0;
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size() ;
+        vector<int> visit(n,0);
+        int ans = 0;
         for(int i =0;i<n;i++){
-            if(parnt[i]==i) ans++;
+            if (visit[i] == 0){ 
+                dfs(visit,isConnected,i);
+                ans++;
+            }
         }
     return ans;
     }
