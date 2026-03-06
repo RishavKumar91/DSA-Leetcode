e1af9aba5265b79cf4c9317d0888722c407bb4ba
RishavKumar91 <rishavyadav15@zohomail.in>
2026-03-06 13:34:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (5.15%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-    vector<vector<int>> vec(m, vector<int>(n, 1));
-    for(int i =1;i<m;i++){
-        for(int j=1;j<n;j++){
-            vec[i][j] = vec[i-1][j] + vec[i][j-1];
-        }
+    int hlpr(int i ,int j,int m,int n , vector<vector<int>> &dp ){
+        if(i==m-1 || j==n-1) return 1;
+        if(dp[i][j] !=-1) return dp[i][j];
+        return dp[i][j] = hlpr(i+1,j,m,n,dp) +hlpr(i,j+1,m,n,dp);
     }
-    return vec[m-1][n-1];
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return hlpr(0,0,m,n,dp);
+
     }
 };
