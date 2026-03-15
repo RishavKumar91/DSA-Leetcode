27c097bdfba0b597ee6cde6626443e70e5f46c5f
RishavKumar91 <rishavyadav15@zohomail.in>
2026-03-15 15:13:06 +0530

    Time: 2 ms (51.62%) | Memory: 8.3 MB (49.43%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
-bool ok(vector<vector<char>> &bor,int row,int col,int n){
-    int i,j;
-    for(i =0;i<row;i++){    // row chck
-        if(bor[i][col]=='Q') return 0;
+bool chck(int row,int col , vector<string> &bor,int &n){
+    int i = row-1 , j = col-1;
+    while(i>=0){
+        if(bor[i][col]=='Q' ) return 0;
+        i--;
     }
-    i=row-1,j=col+1;
-    while(i>=0&&j<n){         // NE
+    i= row-1;
+    j = col-1;
+    while(i>=0 && j>=0){
         if(bor[i][j]=='Q') return 0;
-    i--;j++;
+    j--;
+    i--;
     }
-    i=row-1,j=col-1;
-    while(i>=0&&j>=0){        //NW
+    i = row-1;
+    j = col+1;
+    while(i>=0 && j<n){
         if(bor[i][j]=='Q') return 0;
-    i--;j--;
+        i--;
+        j++;
     }
-    return 1;
+return 1;
 }
-void helper(int n,vector<vector<char>> &bor,int x,int &ans){
-    if(x==n){
+void hlpr(int row ,vector<string> &bor, int &ans,int &n){
+    if(row==n){
         ans++;
         return;
     }
-    for(int j =0;j<n;j++){
-        if(ok(bor,x,j,n)) {
-            bor[x][j] = 'Q';
-            helper(n,bor,x+1,ans);
-            bor[x][j] = '.';
-            }
-    }      
+    for(int i = 0;i<n;i++){
+        if(chck(row,i,bor,n)){
+            bor[row][i]= 'Q';
+            hlpr(row+1,bor,ans,n);
+            bor[row][i] = '.';
+        }
+    }
 }
     int totalNQueens(int n) {
         int ans=0;
-        vector<vector<char>> bor(n, vector<char>(n));
-        for(int i =0;i<n;i++){
-            for(int j =0;j<n;j++){
-                bor[i][j] = '.';
-            }
-        }
-        helper(n,bor,0,ans);
+        vector<string> bor(n, string(n,'.'));
+        hlpr(0,bor,ans,n);
         return ans;
     }
 };
+
