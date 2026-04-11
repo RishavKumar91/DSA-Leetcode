06ab926ce9677b83dd373c0437b25bbba7a72c4a
RishavKumar91 <rishavyadav15@zohomail.in>
2026-04-11 16:29:55 +0530

    Time: 219 ms (88.00%) | Memory: 310.2 MB (82.75%) - LeetSync

--- 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.cpp
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.cpp
@@ -3,12 +3,12 @@ public:
     int minimumDistance(vector<int>& nums) {
         int n = nums.size();
         int ans = INT_MAX ;
-        vector<queue<int>> v(n+1);
+        vector<vector<int>> v(n+1);
         for(int i = 0;i<n;i++){
-            v[nums[i]].push(i);
-            if(v[nums[i]].size() == 3) {
-                ans = min(ans,i-v[nums[i]].front());
-                v[nums[i]].pop();
+            v[nums[i]].push_back(i);
+            if(v[nums[i]].size() >= 3) {
+                vector<int> &anss = v[nums[i]];
+                ans = min(ans,i-anss[anss.size()-3]);
             }
         }
     return ans == INT_MAX ? -1 : 2 * ans;
