96ee8d8fe9a293ec40b04e2ca5b6faad65816965
RishavKumar91 <rishavyadav15@zohomail.in>
2026-06-28 17:02:15 +0530

    Time: 91 ms (27.03%) | Memory: 186.8 MB (30.15%) - LeetSync

--- 4348-maximum-total-sum-of-k-selected-elements/maximum-total-sum-of-k-selected-elements.cpp
+++ 4348-maximum-total-sum-of-k-selected-elements/maximum-total-sum-of-k-selected-elements.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     long long maxSum(vector<int>& nums, int k, int mul) {
-        sort(nums.begin(),nums.end());
+        const int MAX = 100000;
+        int freq[MAX + 1] = {};
+
+        for (int x : nums)
+            freq[x]++;
+
         long long ans = 0;
-        while(k--){
-            int x  = nums.back();
-            nums.pop_back();
-            if(mul>=2) ans += (1LL * mul-- * x);
-            else ans += x;
+
+        for (int val = MAX; val >= 1 && k > 0; val--) {
+            while (freq[val] > 0 && k > 0) {
+                if (mul >= 2)
+                    ans += 1LL * val * mul--;
+                else
+                    ans += val;
+
+                freq[val]--;
+                k--;
+            }
         }
-    return ans;
+
+        return ans;
     }
 };
