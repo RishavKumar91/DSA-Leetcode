a3562bcf50876fd996be4949d437855a1cf0a231
RishavKumar91 <rishavyadav15@zohomail.in>
2026-07-09 13:01:27 +0530

    Time: 572 ms (8.27%) | Memory: 0B (100.00%) - LeetSync

---
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select Person.firstName , Person.lastName ,Address.city , Address.state  from Address right join Person  on Person.personId  = Address.personId ;
