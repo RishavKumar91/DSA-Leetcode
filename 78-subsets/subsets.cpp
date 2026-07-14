fdda19778d1d4470da65797e17618f4d3ee05833
RishavKumar91 <rishavyadav15@zohomail.in>
2026-07-14 15:35:41 +0530

    Time: 3 ms (27.79%) | Memory: 15.8 MB (5.87%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-void helper(vector<vector<int>> &ans,vector<int> &nums,vector<int> v,int idx){
-    if(nums.size()==idx) {
-        ans.push_back(v);
-        return;
+vector<vector<int>> ans;
+void hlpr(vector<int> &nums,vector<int> tmp , int i){
+    if(i==nums.size()) {
+        ans.push_back(tmp);
+        return ;
     }
-    helper(ans,nums,v,idx+1);
-    v.push_back(nums[idx]);
-    helper(ans,nums,v,idx+1);
+    hlpr(nums,tmp,i+1);
+    tmp.push_back(nums[i]);
+    hlpr(nums,tmp,i+1);
 }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> k;
-        vector<int> v;
-        helper(k,nums,v,0);
-        return k;
+        hlpr(nums,{},0);
+        return ans;
     }
 };
