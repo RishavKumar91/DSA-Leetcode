7fddbf9c1160eca18a49b15669c3f45b91e82064
RishavKumar91 <rishavyadav15@zohomail.in>
2026-07-15 18:53:50 +0530

    Time: 3 ms (52.84%) | Memory: 17.9 MB (24.63%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-
-    void solve(int idx, vector<int>& c, int target, vector<int>& temp) {
-        if(target == 0){
-            ans.push_back(temp);
-            return;
-        }
-
-        for(int i = idx; i < c.size(); i++){
-            if(c[i] > target) continue;
-
-            temp.push_back(c[i]);
-            solve(i, c, target - c[i], temp); 
-            temp.pop_back();
-        }
-    }
-
+vector<vector<int>> ans;
+int n;
+void hlpr(vector<int> &c , int tar,int ix,vector<int> &v){
+    if(ix>=n) return ;
+    if(tar<0) return;
+    if(tar==0) { ans.push_back(v); return ;}
+    hlpr(c,tar,ix+1,v);
+    v.push_back(c[ix]);
+    if(tar < c[ix]) { v.pop_back(); return ; } 
+    hlpr(c,tar-c[ix],ix,v); 
+    v.pop_back();
+}
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> temp;
-        solve(0, candidates, target, temp);
-        return ans;
+        sort(candidates.begin(),candidates.end());
+        n = candidates.size();
+        vector<int> v;
+    hlpr(candidates,target,0,v);
+    return ans;
     }
-};
+}; 
