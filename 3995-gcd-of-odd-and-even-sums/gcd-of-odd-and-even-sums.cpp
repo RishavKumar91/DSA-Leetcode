770a4675aefbf4d509ea41fda1c6c3ef7582c914
RishavKumar91 <rishavyadav15@zohomail.in>
2026-07-15 17:49:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.11%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int a = 0 , b = 0 ;
-        for(int i  =  1 ;  i<=n ; i++){
-            a+= i*2 -1 ;
-            b+= i*2;
-        }
-    return __gcd(a,b);
+        // int oddsum = 0 , evensum = 0 ;
+
+        // for(int i  =  1 ;  i<=n ; i++){
+        //     a+= i*2 -1 ;
+        //     b+= i*2;
+        // }
+
+        // oddsum = n*n  ,evensum = n * (n+1);
+
+    // return __gcd(oddsum,evensum);
+    return n;
     }
 };
