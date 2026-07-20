8bc34cae362ac1d21d97c4ad95f5f275558acd20
RishavKumar91 <rishavyadav15@zohomail.in>
2026-07-20 20:17:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (14.45%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        queue<int> q ; 
-        for(int i = 1; i <= 9 ; i++){
+        queue<int> q; vector<int> ans;
+        for(int i = 1 ;i < 9 ; i++){
             q.push(i);
         }
-        vector<int> ans;
         while(!q.empty()){
-            int top = q.front();
+            int now = q.front();
             q.pop();
-            if(top >=low && top <=high) ans.push_back(top);
-            int last = top%10 ; if(last == 9) continue;
-            if(top*10 + last+1 <= high ) q.push(top*10 + last+1);
+            if(now > high) continue;
+            if(now >= low) ans.push_back(now);
+            if(now % 10 == 9) continue;
+            q.push(now*10 + (now%10)+1);
         }
     return ans;
     }
