4cfd4c4017790b65f5b3606da5bd5f2ad5074693
RishavKumar91 <rishavyadav15@zohomail.in>
2026-07-20 20:00:48 +0530

    Time: 4 ms (33.07%) | Memory: 13.5 MB (72.77%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
 vector<string> ans;
-void hlpr(int i ,int j , int n ,string s){
+void hlpr(int i ,int j , int n ,string &s){
     if(i > n) return ;
     if(s.size() == 2*n){
         ans.push_back(s);
         return;
     }
-    hlpr(i+1,j,n,s+"(");
-    if( j < i ) hlpr(i,j+1,n,s+")");
+    if(i<n){
+        s.push_back('(');
+        hlpr(i+1,j,n,s);
+        s.pop_back();
+    }
+    if( j < i ) {
+        s.push_back(')');
+        hlpr(i,j+1,n,s);
+        s.pop_back();
+    }
 }
     vector<string> generateParenthesis(int n) {
-        hlpr(0,0,n , "");
+        string s = "";
+        hlpr(0,0,n , s);
         return ans;
     }
 };
