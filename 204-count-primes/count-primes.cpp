76342e9ce68feddf6814bedde650ad37ba20f4fe
RishavKumar91 <rishavyadav15@zohomail.in>
2026-07-30 00:13:33 +0530

    Time: 167 ms (69.51%) | Memory: 12.7 MB (65.11%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<int> prime(n+1,1);
+        vector<bool> prime(n+1,1);
         prime[0] = 0;
         if(prime.size() >= 2) prime[1] = 0;
         for(int i = 2 ; i*i <=n;i++){
