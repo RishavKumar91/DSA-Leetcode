4c344e0a722dcd54dc4ab5708a88d1bf922f33bc
RishavKumar91 <rishavyadav15@zohomail.in>
2026-07-30 09:25:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (77.73%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        unordered_set<char > st;
-        for(char &ch : word){ 
-            st.insert(ch);
-        }
-        int sz  = st.size();
+        // unordered_set<char > st;
+        // for(char &ch : word){ 
+        //     st.insert(ch);
+        // }
+        int sz  = word.size();
         if(sz <= 8 ) return  sz;
-        else if(sz <= 16) return 8 + 2*(st.size()-8);
-        else if(sz <= 24 ) return 8 + 16 +  3*(st.size()-16) ; 
-        return 8 + 16 +  24 + 4*(st.size()-24) ; 
+        else if(sz <= 16) return 8 + 2*(sz-8);
+        else if(sz <= 24 ) return 8 + 16 +  3*(sz-16) ; 
+        return 8 + 16 +  24 + 4*(sz-24) ; 
     }
 };
