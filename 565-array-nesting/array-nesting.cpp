fa910ef31424f78553e6264d0362f2e05740acaf
RishavKumar91 <rishavyadav15@zohomail.in>
2026-08-02 00:50:28 +0530

    Time: 6 ms (52.62%) | Memory: 99.3 MB (34.72%) - LeetSync

--- 565-array-nesting/array-nesting.cpp
+++ 565-array-nesting/array-nesting.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-int n;
-vector<int> dp ; 
-int hlpr(vector<int>& nums , int i,vector<bool> &visit){
-    if(visit[nums[i]]) return 0;
-    if(dp[i] != -1) return dp[i];
-    visit[nums[i]] = 1;
-    return dp[i] =  1 + hlpr(nums,nums[i],visit);
-}
     int arrayNesting(vector<int>& nums) {
-        n = nums.size();
-        int ans = 0 ;
-        dp.resize(n+1,-1);
-        vector<bool> visit(n+1,0);
-        for(int i = 0 ; i < n ; i++){
-            if(visit[nums[i]]) continue;
-            ans = max(ans,hlpr(nums,nums[i],visit));
+        int n = nums.size();
+        int ans = 0;
+        vector<int> visit(n,0);
+        for(int i = 0 ; i < n ;  i++){
+            if(visit[i]) continue;
+            int count = 0 , curr = i;
+            while(!visit[curr] ){
+                count++;
+                visit[curr] = 1;
+                curr = nums[curr];
+            }
+            ans = max(ans,count);
         }
-        return ans ; 
+    return ans;
     }
 };
