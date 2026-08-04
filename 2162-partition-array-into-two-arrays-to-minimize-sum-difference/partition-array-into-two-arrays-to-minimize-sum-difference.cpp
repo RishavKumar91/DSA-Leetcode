2056ad01e08bedfb9f1e91cc1118858207c41a9b
RishavKumar91 <rishavyadav15@zohomail.in>
2026-08-04 11:23:55 +0530

    Time: 441 ms (40.75%) | Memory: 73.3 MB (90.34%) - LeetSync

--- 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -1,70 +1,63 @@
 class Solution {
 public:
     int minimumDifference(vector<int>& nums) {
-        int N = nums.size() , SUM = accumulate(nums.begin() , nums.end() , 0 );
-        int n = N/2;
-        unordered_map<int,vector<int> > mp1 , mp2;
-        for(int mask = 0 ; mask < (1<<n) ; mask++){
-            int sum = 0 , count = 0 ;
-            for(int i = 0 ; i < n ; i++){
-                if(mask & (1<<i)){
-                    count++ ; sum+=nums[i];
+        int N = nums.size();
+        int n = N / 2;
+        int SUM = accumulate(nums.begin(), nums.end(), 0);
+
+        vector<vector<int>> left(n + 1), right(n + 1);
+
+        for (int mask = 0; mask < (1 << n); mask++) {
+            int cnt = 0, sum = 0;
+            for (int i = 0; i < n; i++) {
+                if (mask & (1 << i)) {
+                    cnt++;
+                    sum += nums[i];
                 }
             }
-        mp1[count].push_back(sum);
+            left[cnt].push_back(sum);
         }
-        for(int mask = 0 ; mask < (1<<n) ; mask++){
-            int sum = 0 , count = 0 ;
-            for(int i = 0 ; i < n ; i++){
-                if(mask & (1<<i)){
-                    count++ ; sum+=nums[n+i];
+
+        for (int mask = 0; mask < (1 << n); mask++) {
+            int cnt = 0, sum = 0;
+            for (int i = 0; i < n; i++) {
+                if (mask & (1 << i)) {
+                    cnt++;
+                    sum += nums[n + i];
                 }
             }
-        mp2[count].push_back(sum);
+            right[cnt].push_back(sum);
         }
 
-    int ans = INT_MAX ; 
-        for(auto &x : mp2) sort(x.second.begin() , x.second.end() ) ; 
+        for (int i = 0; i <= n; i++)
+            sort(right[i].begin(), right[i].end());
 
-        for(auto &m1 : mp1){
-            int currcount = m1.first ;
-            int fincount = n - currcount ;
-            for(auto &mm1 : m1.second){
-                int currsubsum = mm1;
-                auto &m2  =  mp2[fincount] ;
-                    int f = SUM / 2 - currsubsum;
-                    int l = lower_bound(m2.begin()  ,  m2.end()  , f) - m2.begin();
+        int ans = INT_MAX;
 
-                    if(l >=0 && l< mp2[fincount].size()) ans = min(ans ,  abs(SUM - 2*(currsubsum + mp2[fincount][l]))) ; 
+        for (int cnt = 0; cnt <= n; cnt++) {
+            auto &A = left[cnt];
+            auto &B = right[n - cnt];
 
-                    if(l-1 >=0 && l-1 < mp2[fincount].size()) ans = min(ans ,  abs(SUM - 2*(currsubsum + mp2[fincount][l-1]))) ;
-                    if(ans == 0 ) return ans;
+            for (int s1 : A) {
+                int target = SUM / 2 - s1;
+
+                auto it = lower_bound(B.begin(), B.end(), target);
+
+                if (it != B.end()) {
+                    int total = s1 + *it;
+                    ans = min(ans, abs(SUM - 2 * total));
+                }
+
+                if (it != B.begin()) {
+                    --it;
+                    int total = s1 + *it;
+                    ans = min(ans, abs(SUM - 2 * total));
+                }
+
+                if (ans == 0) return 0;
             }
         }
-    return ans ; 
+
+        return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-// DO $$
-// DECLARE
-// AGE INT:=16;
-// BEGIN
-// 	IF AGE>=18 THEN
-// 	RAISE NOTICE 'YOU ARE ELIGIBLE FOR A JOB BECAUSE YOUR AGE IS %',AGE;
-// 	ELSE
-// 	RAISE NOTICE 'YOU ARE NOT ELIGIBLE FOR A JOB BECAUSE YOUR AGE IS LESS THE 18';
-// 	END IF;
-		
-// END $$;
