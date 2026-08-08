c5f4ec40bf24399d00bb5922b853304451f4b63e
RishavKumar91 <rishavyadav15@zohomail.in>
2026-08-09 02:41:50 +0530

    Time: 7 ms (17.00%) | Memory: 16.2 MB (7.47%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,44 +1,43 @@
 class Solution {
 public:
-bool chck(int row,int col , vector<string> &bor,int &n){
+vector<vector<string>> ans;
+bool chck(int row , int col , vector<string> &bor){
+    int n  = bor.size();
+    for(int i = 0 ; i < row ; i++){
+        if(bor[i][col] == 'Q') return 0;
+    }
+    for(int i = 0 ; i < col ; i++){
+        if(bor[row][i] == 'Q') return 0;
+    }
     int i = row-1 , j = col-1;
-    while(i>=0){
-        if(bor[i][col]=='Q' ) return 0;
-        i--;
+    while(i >= 0 && j >= 0){
+        if(bor[i][j] == 'Q') return 0;
+        i--; j--;
     }
-    i= row-1;
-    j = col-1;
-    while(i>=0 && j>=0){
-        if(bor[i][j]=='Q') return 0;
-    j--;
-    i--;
+    i = row-1 , j = col+1;
+    while(i >= 0  && j < n){
+        if(bor[i][j] == 'Q') return 0;
+        i--; j++;
     }
-    i = row-1;
-    j = col+1;
-    while(i>=0 && j<n){
-        if(bor[i][j]=='Q') return 0;
-        i--;
-        j++;
-    }
-return 1;
+    return 1;
 }
-void hlpr(int row ,vector<string> &bor, vector<vector<string>> &ans,int &n){
-    if(row==n){
+void backtrack(vector<string> bor , int row ){
+    int n = bor.size();
+    if(n==row){
         ans.push_back(bor);
-        return;
+        return ;
     }
-    for(int i = 0;i<n;i++){
-        if(chck(row,i,bor,n)){
-            bor[row][i]= 'Q';
-            hlpr(row+1,bor,ans,n);
+    for(int i = 0 ; i < n ; i++){
+        if(chck(row,i,bor)){
+            bor[row][i] = 'Q';
+            backtrack(bor,row+1);
             bor[row][i] = '.';
         }
     }
 }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-        vector<string> bor(n, string(n,'.'));
-        hlpr(0,bor,ans,n);
+        vector<string> bor(n,string(n,'.'));
+        backtrack(bor,0);
         return ans;
     }
 };
