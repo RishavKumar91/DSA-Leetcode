13e9e3969238699e2eeeb9181996d885a40b5725
RishavKumar91 <rishavyadav15@zohomail.in>
2026-08-09 01:53:56 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (69.66%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+bool chck(vector<vector<char>>& board , int row , int col){
+    int arr[10] = {0};
+    for(int i = 0 ;  i < 9 ; i++){
+        char ch = board[i][col];
+        if(ch != '.'){
+            if(arr[ch-'0'] == 1) return 0;
+            arr[ch-'0']++;
+        }
+    }
+    memset(arr,0,sizeof(arr));
+    for(int i = 0 ;  i < 9 ; i++){
+        char ch = board[row][i];
+        if(ch != '.'){
+            if(arr[ch-'0'] == 1) return 0;
+            arr[ch-'0']++;
+        }
+    }
+    memset(arr,0,sizeof(arr));
+    row = (row/3)*3 , col = (col/3)*3 ;
+    for(int i = row ; i < row+3 ; i++){
+        for(int j = col ; j < col + 3 ; j++){
+            char ch = board[i][j];
+            if(ch != '.'){
+                if(arr[ch-'0'] == 1) return 0;
+                arr[ch-'0']++;
+            }
+        }
+    }
+
+return 1;
+}
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(int i = 0 ; i < 9 ; i++){
+            for(int j = 0 ; j < 9 ; j++){
+                if(board[i][j] == '.') continue;
+                if(!chck(board,i,j)) return 0;
+            }
+        }
+    return 1;
+    }
+};
