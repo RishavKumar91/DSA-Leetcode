3150900a513b3886f9cb29bad4dce80972947187
RishavKumar91 <rishavyadav15@zohomail.in>
2026-08-09 01:54:38 +0530

    Time: 3 ms (55.28%) | Memory: 22.2 MB (66.83%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
 bool chck(vector<vector<char>>& board , int row , int col){
-    int arr[10] = {0};
+    int arr[9] = {0};
     for(int i = 0 ;  i < 9 ; i++){
         char ch = board[i][col];
         if(ch != '.'){
-            if(arr[ch-'0'] == 1) return 0;
-            arr[ch-'0']++;
+            if(arr[ch-'1'] == 1) return 0;
+            arr[ch-'1']++;
         }
     }
     memset(arr,0,sizeof(arr));
     for(int i = 0 ;  i < 9 ; i++){
         char ch = board[row][i];
         if(ch != '.'){
-            if(arr[ch-'0'] == 1) return 0;
-            arr[ch-'0']++;
+            if(arr[ch-'1'] == 1) return 0;
+            arr[ch-'1']++;
         }
     }
     memset(arr,0,sizeof(arr));
@@ -23,8 +23,8 @@ bool chck(vector<vector<char>>& board , int row , int col){
         for(int j = col ; j < col + 3 ; j++){
             char ch = board[i][j];
             if(ch != '.'){
-                if(arr[ch-'0'] == 1) return 0;
-                arr[ch-'0']++;
+                if(arr[ch-'1'] == 1) return 0;
+                arr[ch-'1']++;
             }
         }
     }
