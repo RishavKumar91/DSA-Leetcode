30dc7fe7f713459cb4e54e1c6971564d1a92fa31
RishavKumar91 <rishavyadav15@zohomail.in>
2026-08-10 15:16:07 +0530

    Time: 44 ms (47.57%) | Memory: 16.6 MB (45.73%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -3,14 +3,14 @@ public:
 vector<int> p;
     bool hlpr(int n ){
         if(n == 0) return 0;
-        if(p[n] == 1) return p[n];
+        if(p[n] != -1) return p[n];
         for(int i = 1 ;  i * i <= n ; i++){
             if(!hlpr(n-(i*i))  ) return p[n] =   1;
         }
-    return 0 ;
+    return p[n] = 0 ;
     }
     bool winnerSquareGame(int n) {
-        p.resize(n+1,0);
+        p.resize(n+1,-1);
         return hlpr(n);
     }
 };
