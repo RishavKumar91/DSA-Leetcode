0c23b3403f85ea060d129dcb3f1b1fa330798d73
RishavKumar91 <rishavyadav15@zohomail.in>
2026-08-16 03:09:58 +0530

    Time: 269 ms (49.32%) | Memory: 11 MB (44.90%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
 int m , n ;
     vector<pair<int,int>> dxn = {{0,1},{0,-1},{1,0},{-1,0}};
-    bool hlpr(vector<vector<char>>& board, string word , int a ,int i,int j,vector<vector<bool>> &visit){
+    bool hlpr(vector<vector<char>>& board, string &word , int a ,int i,int j,vector<vector<bool>> &visit){
         if(board[i][j] != word[a]) return 0;
         if(a == word.size()-1){
             return 1;
         }
+        visit[i][j] = 1;
         for(auto &d : dxn){
             int ni = i + d.first, nj = j + d.second;
             if(ni >= m || nj >=n || ni < 0 || nj < 0 || visit[ni][nj]) continue;
-            visit[i][j] = 1;
-            if(hlpr(board,word,a+1,ni,nj,visit)) return 1;
-            visit[i][j] = 0;
+
+            if(hlpr(board,word,a+1,ni,nj,visit)) return 1; 
         }
+        visit[i][j] = 0;
     return 0;
     }
     bool exist(vector<vector<char>>& board, string word) {
