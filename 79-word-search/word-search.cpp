b23372ad8935a979de8f32c388aba28ad41a46e9
RishavKumar91 <rishavyadav15@zohomail.in>
2026-08-16 00:27:57 +0530

    Time: 550 ms (33.06%) | Memory: 12.1 MB (6.81%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-int m , n;
-bool backtrack(int i,int j,vector<vector<char>> &board,string &word,int ix){
-    if(board[i][j]=='\0' || board[i][j] != word[ix]) return 0;
-    if(ix==word.size()-1) return 1;
-    char tmp = board[i][j];
-    board[i][j] = '\0';
-    if( (i+1<m && backtrack(i+1,j,board,word,ix+1) )||
-        (j+1<n && backtrack(i,j+1,board,word,ix+1) )||
-        (j-1>=0 && backtrack(i,j-1,board,word,ix+1) )||
-        (i-1>=0 && backtrack(i-1,j,board,word,ix+1))   )   return 1;
-    board[i][j] = tmp;
-return 0;
-}
-    bool exist(vector<vector<char>>& board, string word) {
-        m = board.size();
-        n = board[0].size();
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-               if(word[0] ==board[i][j] && backtrack(i,j,board,word,0)) return 1;
-            }
+int m , n ;
+    vector<pair<int,int>> dxn = {{0,1},{0,-1},{1,0},{-1,0}};
+    bool hlpr(vector<vector<char>>& board, string word , int a ,int i,int j,vector<vector<bool>> &visit){
+        if(board[i][j] != word[a]) return 0;
+        if(a == word.size()-1){
+            return 1;
+        }
+        for(auto &d : dxn){
+            int ni = i + d.first, nj = j + d.second;
+            if(ni >= m || nj >=n || ni < 0 || nj < 0 || visit[ni][nj]) continue;
+            visit[i][j] = 1;
+            if(hlpr(board,word,a+1,ni,nj,visit)) return 1;
+            visit[i][j] = 0;
         }
     return 0;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        m = board.size() , n = board[0].size();
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(board[i][j] == word[0]) {
+                    vector<vector<bool>> visit(m+1,vector<bool> (n+1,0));
+                    visit[i][j] = 1;
+                    if(hlpr(board,word,0,i,j,visit)){
+                        return 1;
+                    }
+                    visit[i][j] = 0;
+                }
+            }
+        }
+        return 0;
     }
 };
