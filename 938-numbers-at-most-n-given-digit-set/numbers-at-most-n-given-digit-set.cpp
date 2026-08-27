552fff21a8a54a51534d576ec9afa2c57bf16208
RishavKumar91 <rishavyadav15@zohomail.in>
2026-08-28 05:23:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (31.07%) - LeetSync

--- 938-numbers-at-most-n-given-digit-set/numbers-at-most-n-given-digit-set.cpp
+++ 938-numbers-at-most-n-given-digit-set/numbers-at-most-n-given-digit-set.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
 int mask ; 
 string s;
+vector<vector<vector<int>>> dp ; 
     int hlpr(int ix , bool tight , bool LZ){
         if( ix == s.size()) return 1;
+        if( dp[ix][tight][LZ] != -1 ) return dp[ix][tight][LZ] ;
         int limit = tight ? s[ix]-'0' : 9;
         int ans  = 0 ;
         for(int d = 0 ; d <= limit ; d++){
@@ -14,12 +16,14 @@ string s;
             
                 ans += hlpr(ix+1,tight && d==s[ix]-'0', LZ && d==0 );
         }
-    return ans;
+    return dp[ix][tight][LZ] =  ans;
     }
     int atMostNGivenDigitSet(vector<string>& digits, int n) {
         mask = 0 ;
         s = to_string(n);
         for(auto &s : digits) mask = mask | 1<<stoi(s);
+        dp.resize(s.size()+1,vector<vector<int>> (2,vector<int> (2,-1)));
+
         return hlpr(0,1,1)- 1;
     }
 };
