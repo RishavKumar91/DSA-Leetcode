7198f44ff1d6db3f22f7bf7ac553a7b8e295349c
RishavKumar91 <rishavyadav15@zohomail.in>
2026-09-08 22:07:39 +0530

    Time: 43 ms (22.81%) | Memory: 34.7 MB (14.52%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
 int n;
 vector<vector<int>> dp ;
-    bool chck(int i , int j , string &s){
-        int ii = i , jj = j ;
-        if(dp[i][j] != -1) return dp[ii][jj] ;
-        while(i<j ){
-            if(s[i] != s[j]) return dp[ii][jj] = 0;
-            i++;
-            j--;
-        }
-    return dp[ii][jj] = 1;
-    }
+    bool chck(int i, int j, string &s){
+    if(i >= j) return true;
+
+    if(dp[i][j] != -1)
+        return dp[i][j];
+
+    if(s[i] != s[j])
+        return dp[i][j] = 0;
+
+    return dp[i][j] = chck(i + 1, j - 1, s);
+}
     int countSubstrings(string s) {
         n = s.size();
-        dp.resize(n+1,vector<int> (n+1,-1));
+        dp.assign(n+1,vector<int> (n+1,-1));
         int ans = 0 ;
         for(int i = 0 ; i < n ; i++){
             for(int j = i ; j < n ; j++){
