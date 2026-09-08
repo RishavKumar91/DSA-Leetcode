5d7629374db502d49d75fe7967aa3119b2500910
RishavKumar91 <rishavyadav15@zohomail.in>
2026-09-08 23:31:19 +0530

    Time: 56 ms (41.12%) | Memory: 80 MB (23.96%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+vector<vector<string>> ans;
+int n;
+    bool ispalindrome(string &s , int i , int j){
+        while(i<j){
+            if(s[i] != s[j]) return 0;
+            i++;
+            j--;
+        }
+    return 1;
+    }
+    void hlpr(vector<string> &st ,string &s, string &tmp , int startix){
+        if(startix == n){
+            ans.push_back(st);
+            return ;
+        }
+        for(int lastix = startix ; lastix < n ; lastix++){
+            if(!ispalindrome(s,startix,lastix)) continue;
+            st.push_back(s.substr(startix,lastix-startix+1));
+            hlpr(st,s,tmp,lastix+1);
+            st.pop_back();
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        string tmp ;
+        n = s.size();
+        vector<string> st;
+        hlpr(st,s,tmp,0);
+        return ans;
+    }
+};
