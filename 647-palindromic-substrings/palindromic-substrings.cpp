9df7032e83a0d5faaac08e876aa679d3fe6e97a2
RishavKumar91 <rishavyadav15@zohomail.in>
2026-09-08 22:38:29 +0530

    Time: 3 ms (86.04%) | Memory: 8.6 MB (59.67%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
 int n;
-vector<vector<int>> dp ;
-    bool chck(int i, int j, string &s){
-    if(i >= j) return true;
-
-    if(dp[i][j] != -1)
-        return dp[i][j];
-
-    if(s[i] != s[j])
-        return dp[i][j] = 0;
-
-    return dp[i][j] = chck(i + 1, j - 1, s);
-}
+    int hlpr(int i , int j , string &s){
+        int count = 0 ;
+        while(i >= 0 && j < n && s[i]==s[j]){
+            i--;
+            j++;
+            count++;
+        }
+        return count;
+    }
     int countSubstrings(string s) {
         n = s.size();
-        dp.assign(n+1,vector<int> (n+1,-1));
         int ans = 0 ;
         for(int i = 0 ; i < n ; i++){
-            for(int j = i ; j < n ; j++){
-                if(chck(i,j,s)) ans++;
-            }
+            ans += hlpr(i,i,s);
+            ans += hlpr(i,i+1,s);
         }
     return ans;
     }
