e1eeaad0a04182ebc068b92004f94bd260596af6
RishavKumar91 <rishavyadav15@zohomail.in>
2026-09-08 20:31:00 +0530

    Time: 35 ms (39.30%) | Memory: 57.3 MB (19.76%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 int n ;
-    string hlpr(int i , int j , string &s){
+    pair<int,int> hlpr(int i , int j , string &s){
         while(i >= 0 && j < n ){
             if(s[i] == s[j]) {
                 i--;
@@ -11,16 +11,18 @@ int n ;
                 break;
             }
         }
-    return s.substr(i+1,j-i-1); // i+1 se j-1 tak ,  so ln = (j-1) - (i+1) + 1 = j-i-1
+    return {i+1 , j-i-1}; // i+1 se j-1 tak ,  so ln = (j-1) - (i+1) + 1 = j-i-1
     }
     string longestPalindrome(string s) {
         n = s.size();
         string ans = "";
         for(int i = 0 ; i < n ; i++){
-            string oans = hlpr(i,i,s);
-            string eans = hlpr(i,i+1,s);
-            ans = oans.size() > ans.size() ? oans : ans ; 
-            ans = eans.size() > ans.size() ? eans : ans ; 
+            pair<int,int> oans = hlpr(i,i,s);
+            pair<int,int> eans = hlpr(i,i+1,s);
+            // string eans = hlpr(i,i+1,s);
+            ans = oans.second > ans.size() ? s.substr(oans.first,oans.second) : ans ; 
+            ans = eans.second > ans.size() ? s.substr(eans.first,eans.second) : ans ; 
+            // ans = eans.size() > ans.size() ? eans : ans ; 
         }
     return ans;
     }
