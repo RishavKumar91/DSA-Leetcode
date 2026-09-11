5ec33e74c36c58e82bcbd6ab97e948601308d60c
RishavKumar91 <rishavyadav15@zohomail.in>
2026-09-12 02:36:29 +0530

    Time: 0 ms (100.00%) | Memory: 29.7 MB (79.21%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -3,24 +3,21 @@ public:
     int totalNumbers(vector<int>& digits) {
         int n = digits.size();
         int ans = 0 ;
-        unordered_set<int> st;
+        bool st[1000] = {0};
+        // unordered_set<int> st;
         for(int i = 0 ; i < n ; i++){
             if(!digits[i]  ) continue;
             for(int j = 0 ; j < n ; j++){
                 if( i == j ) continue;
                 for(int k = 0 ; k < n ; k++){
                     if( j==k || i==k || digits[k]&1 ) continue;
-                    st.insert(digits[i]*100 + digits[j]*10 + digits[k]);
+                    if(!st[digits[i]*100 + digits[j]*10 + digits[k]]){
+                        ans++;
+                        st[digits[i]*100 + digits[j]*10 + digits[k]] = 1;
+                    }
                 }
             }
         }
-    return st.size();
+    return ans;
     }
 };
-
-
-
-
-
-
-
