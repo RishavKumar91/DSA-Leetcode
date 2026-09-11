c5f9f688867b86f0db4a7896bbe4d7ef18ba54fc
RishavKumar91 <rishavyadav15@zohomail.in>
2026-09-12 00:45:52 +0530

    Time: 94 ms (53.90%) | Memory: 63 MB (6.76%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     int n;
     vector<int> dp ; 
+    vector<vector<int>> palindp ;
     bool ispalindrome(int i , int j , string &s){
+        if(palindp[i][j] != -1) return palindp[i][j];
         if(i >=j ) return 1;
         if(s[i] != s[j]) return 0;
-    return ispalindrome(i+1,j-1,s);
+    return palindp[i][j] =  ispalindrome(i+1,j-1,s);
     }
     int hlpr(int i , string &s){
         if(i==n) return -1;
@@ -20,6 +22,7 @@ public:
     }
     int minCut(string s) {
         n = s.size();
+        palindp.resize(n+1,vector<int> (n+1,-1));
         dp.resize(n+1,-1);
         return hlpr(0,s);
     }
