bdd07a8d00e885a5a6585705bc609f11494d3ee1
RishavKumar91 <rishavyadav15@zohomail.in>
2026-09-15 18:44:17 +0530

    Time: 591 ms (12.04%) | Memory: 514 MB (5.76%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -18,7 +18,7 @@ vector<vector<int>> dpal;
     // }
     int hlpr(string &s , int k , int i , int j){
         if(i >= n) return 0;
-        if(j >= n) return  hlpr(s,k,i+1,i+1);
+        if(j >= n) return  hlpr(s,k,i+1,i+k-1);
         if(dp[i][j] != -1) return dp[i][j] ; 
         if( !dpal[i][j] || j-i+1 < k ) return dp[i][j] =  hlpr(s,k,i,j+1) ;
         if(dpal[i][j] ) return dp[i][j] = max( 1 + hlpr(s,k,j+1,j+k) , hlpr(s,k,i,j+1) ) ;
