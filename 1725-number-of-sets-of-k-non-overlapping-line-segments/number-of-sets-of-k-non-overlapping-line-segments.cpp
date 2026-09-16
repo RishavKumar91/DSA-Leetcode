21c17ce09cfe836be1cd139f1f37a89236cec819
RishavKumar91 <rishavyadav15@zohomail.in>
2026-09-16 21:28:07 +0530

    Time: 38 ms (61.21%) | Memory: 34.7 MB (39.25%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
     int M = 1e9 + 7;
-    vector<vector<int>> dp;
-    vector<vector<int>> suf;
+    int dp[1001][1001];
 
-    int hlpr(int ix, int n, int k) {
-        if(k == 0) return 1;
-        if(ix >= n) return 0;
+    int numberOfSets(int n, int K) {
 
-        if(dp[ix][k] != -1)
-            return dp[ix][k];
-
-        int skip = hlpr(ix + 1, n, k);
-
-        // original:
-        // for(int i = ix + 1; i < n; i++)
-        //     take += hlpr(i,n,k-1);
-
-        int take = suf[ix + 1][k - 1];
-
-        return dp[ix][k] = (skip + take) % M;
-    }
-
-    int numberOfSets(int n, int k) {
-        dp.assign(n + 1, vector<int>(k + 1, -1));
-        suf.assign(n + 1, vector<int>(k + 1, 0));
-
-        // k = 0 -> every ix has exactly 1 way
-        for(int i = n - 1; i >= 0; i--) {
-            suf[i][0] = (suf[i + 1][0] + 1) % M;
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = (i < n) ? 1 : 0;
         }
 
-        // Build suffix sums for all k
-        for(int j = 1; j <= k; j++) {
-            for(int i = n - 1; i >= 0; i--) {
-                suf[i][j] = (suf[i + 1][j] + hlpr(i, n, j)) % M;
+        for (int k = 1; k <= K; k++) {
+
+            vector<int> prevRowSum(n + 1, 0);
+
+            //prevRowSum[x] = dp[k-1][x] + dp[k-1][x+1] + ... + dp[k-1][n-1]
+            for (int x = n - 1; x >= 0; x--) {
+                prevRowSum[x] = (prevRowSum[x + 1] + dp[k - 1][x]) % M;
+            }
+
+            for (int i = n - 1; i >= 0; i--) {
+                
+                int skip = dp[k][i + 1];
+
+                int take = prevRowSum[i+1];
+
+                dp[k][i] = (take + skip) % M;
             }
         }
 
-        return hlpr(0, n, k);
+        return dp[K][0];
     }
 };
