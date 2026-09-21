c6d80c413feba6c61eaf4476534745fc6475449a
RishavKumar91 <rishavyadav15@zohomail.in>
2026-09-22 01:44:51 +0530

    Time: 2 ms (12.46%) | Memory: 11.8 MB (53.81%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
-        vector<vector<int>> ans(m, vector<int>(n,0));
-        if(obstacleGrid[0][0]==1) return 0;
-        ans[0][0] =1;
-        for(int i =0;i<m;i++){
-            for(int j =0;j<n;j++){
-                if(obstacleGrid[i][j]==1) ans[i][j]=0;
-                else {
-                    if(i>0) ans[i][j] += ans[i-1][j] ;
-                    if(j>0) ans[i][j] +=ans[i][j-1];
-                }
-            }
-        }
-    return ans[m-1][n-1];
+int m , n ;
+vector<vector<int>> dp;
+    int hlpr(int i , int j, vector<vector<int>>& obs){
+        if(i < 0 || j < 0 || obs[i][j] == 1 ) return 0;
+        if( i == 0 && j == 0) return 1;
+        if(dp[i][j] != -1) return dp[i][j] ;
+        return dp[i][j] = hlpr(i-1,j,obs) + hlpr(i,j-1,obs);
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obs) {
+        m = obs.size();
+        n = obs[0].size();
+        dp.resize(m+1,vector<int> (n+1,-1));
+        return hlpr(m-1,n-1,obs);
     }
 };
