8e55b8ebd88615f50a814ae1f36cd8585e7a03c8
RishavKumar91 <rishavyadav15@zohomail.in>
2026-09-25 00:01:09 +0530

    Time: 2 ms (27.45%) | Memory: 10.4 MB (21.51%) - LeetSync

--- 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
+++ 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
@@ -5,10 +5,10 @@ vector<int> dp ;
         if(n==1) return 0;
         if(dp[n] != -1) return dp[n];
         int ans  = INT_MAX ;
-        for(int i = 1 ; i < n; i++){
-            ans = min(ans,hlpr(i)  + hlpr(n-1) + i*n-i);
-        }
-    return dp[n] =  ans;
+        // for(int i = 1 ; i < n; i++){
+        //     ans = min(ans,hlpr(i)  + hlpr(n-1) + i*n-i);
+        // }
+    return dp[n] = hlpr(1)  + hlpr(n-1) + n-1 ;
     }
     int minCost(int n) {
         dp.resize(n+1,-1);
